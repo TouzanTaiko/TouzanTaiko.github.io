e576c9cfceee790d27d30791ee3772995b100210
AHChoi <ahchoi@cpp.edu>
2017-03-17 22:25:32 -0700

    Add files via upload

---
+++ js/vitality.js
@@ -0,0 +1,140 @@
+/*!
+ * Vitality v1.4.0 (http://themes.startbootstrap.com/vitality-v1.4.0)
+ * Copyright 2013-2017 Start Bootstrap Themes
+ * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
+ */
+// Load WOW.js on non-touch devices
+var isPhoneDevice = "ontouchstart" in document.documentElement;
+$(document).ready(function() {
+    if (isPhoneDevice) {
+        //mobile
+    } else {
+        //desktop               
+        // Initialize WOW.js
+        wow = new WOW({
+            offset: 50
+        })
+        wow.init();
+    }
+});
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($("nav.navbar").offset().top > 200) {
+            $("nav.navbar").removeClass("navbar-expanded");
+        } else {
+            $("nav.navbar").addClass("navbar-expanded");
+        }
+    });
+
+    // Smooth Scrolling: Smooth scrolls to an ID on the current page.
+    // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Activates floating label headings for the contact form.
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Owl Carousel Settings
+    $(".about-carousel").owlCarousel({
+        items: 3,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+    });
+
+    $(".portfolio-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        autoHeight: true,
+        mouseDrag: false,
+        touchDrag: false,
+        transitionStyle: "fadeUp"
+    });
+
+    $(".testimonials-carousel, .mockup-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: true,
+        autoHeight: true,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        transitionStyle: "backSlide"
+    });
+
+    $(".portfolio-gallery").owlCarousel({
+        items: 3,
+    });
+
+    // Magnific Popup jQuery Lightbox Gallery Settings
+    $('.gallery-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        image: {
+            titleSrc: 'title'
+        }
+    });
+
+    $('.mix').magnificPopup({
+        type: 'image',
+        image: {
+            titleSrc: 'title'
+        }
+    });
+
+    // Vide - Video Background Settings
+    $('header.video').vide({
+        mp4: "mp4/camera.mp4",
+        poster: "img/agency/backgrounds/bg-mobile-fallback.jpg"
+    }, {
+        posterType: 'jpg'
+    });
+
+    // Scrollspy: Highlights the navigation menu items while scrolling.
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Portfolio Filtering Scripts & Hover Effect
+    var filterList = {
+        init: function() {
+            // MixItUp plugin
+            $('#portfoliolist').mixItUp();
+        },
+    };
+
+    filterList.init();
+
+})(jQuery); // End of use strict
